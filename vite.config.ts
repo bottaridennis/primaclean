7b19f7787581ecb4ae05dfae4304921497022461
Dennis Bottari <dennisbottari@gmail.com>
2026-04-23 01:16:48 +0200

    v4

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: './', // Serve assets with relative paths instead of absolute (vital for GitHub Pages subfolders)
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
